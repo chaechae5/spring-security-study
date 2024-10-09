2af5a7a5dbc803f37c77b896799c34c29f6055ea
eunchae oh <eunchae1083@gmail.com>
2024-10-09 18:18:50 +0900

    엔드포인트 권한 부여 구성

--- src/main/java/com/chae/security_study/config/ProjectConfig.java
+++ src/main/java/com/chae/security_study/config/ProjectConfig.java
@@ -2,11 +2,14 @@ package com.chae.security_study.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 /*
 * UserDetailService 구현을 재정의하는 방법을 알아보기 위한 예제
@@ -34,5 +37,21 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    /*
+    * spring Security 5.7부터는 WebSecurityConfigurerAdapter deprecated
+    * */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // HTTP Basic 인증을 활성화합니다.
+                .httpBasic(Customizer.withDefaults()) // Deprecated된 httpBasic() 대신 사용
+                // 모든 요청에 대해 인증 없이 접근 허용
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 }
